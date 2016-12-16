751bfcb27c70e95f8c04710f25cba81d128af7a1
byungmin_Kim <it_wangchun@naver.com>
2016-12-16 11:45:41 +0900

    add study sorce

---
+++ frontend/study_underscoreSorce.js
@@ -0,0 +1,10 @@
+//     Underscore.js 1.8.3
+//     http://underscorejs.org
+//     (c) 2009-2016 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+//     Underscore may be freely distributed under the MIT license.
+
+// 개인적으로 javascript공부할겸 underscorejs 소스를 보고 있습니다.
+
+var root = typeof self == 'object' && self.self === self && self ||
+            typeof global == 'object' && global.global === global && global ||
+            this;
