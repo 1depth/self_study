22d6bc99fe5f3309977437b0adf9ce7314c5aaa9
byungmin_Kim <it_wangchun@naver.com>
2016-12-16 12:18:33 +0900

    fix text

--- frontend/study_underscoreSorce.js
+++ frontend/study_underscoreSorce.js
@@ -3,7 +3,7 @@
 //     (c) 2009-2016 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
 //     Underscore may be freely distributed under the MIT license.
 
-// 개인적으로 javascript공부할겸 underscorejs 소스를 보고 있습니다.
+//     개인적으로 javascript공부할겸 underscorejs 소스를 보고 있습니다.
 
 (function(){
     
