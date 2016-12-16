51f5cce959c7c18ce98b6cc30237b3bd8f2e73cd
byungmin_Kim <it_wangchun@naver.com>
2016-12-16 12:11:39 +0900

    add source with develop branch

--- frontend/study_underscoreSorce.js
+++ frontend/study_underscoreSorce.js
@@ -11,7 +11,7 @@
     var root = typeof self == 'object' && self.self === self && self ||
             typeof global == 'object' && global.global === global && global ||
             this;
-    
+    var previousUnderscore = root._
 
 
 }());
