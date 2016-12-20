8c64e8b0e65a610a60a3b7324333436d6b14facc
byungmin_kim <whoilgga@gmail.com>
2016-12-20 18:11:02 +0900

    add source

--- frontend/study_underscoreSorce.js
+++ frontend/study_underscoreSorce.js
@@ -593,6 +593,186 @@
 		return result;
 	};
 
+	var executeBound = function(sourceFunc, boundFunc, context, callingContext, args) {
+		if (!(callingContext instanceof boundFunc)) return sourceFunc.apply(context, args);
+		var self = baseCreate(sourceFunc.prototype);
+		var result = sourceFunc.apply(self, args);
+		if (_.isObject(result)) return result;
+		return self;
+	};
+
+	_.bind = restArgs(function(func, context, args) {
+		if (!_.isFunction(func)) throw new TypeError ('Bind must be called on a function');
+		var bound = restArgs(function(callArgs) {
+			return executeBound(func, bound, context, this, args.concat(allArgs));
+		});
+		return bound;
+	});
+
+	_.partial = restArgs(function(func, boundArgs) {
+		var placeholder = _.partial.placeholder;
+		var bound = function() {
+			var position = 0, length = boundArgs.length;
+			var args = Array(length);
+			for (var i = 0; i < length; i++) {
+				args[i] = boundArgs[i] === placeholder ? arguments[position++] : boundArgs[i];
+			}
+			while (position < arguments.length) args.push(arguments[position++]);
+			return executeBound(func, bound, this, this, args);
+		};
+		return bound;
+	});
+
+	_.partial.placeholder = _;
+
+	_.bindAll = restArgs(function(obj, keys) {
+		keys = flatten(keys, false, false);
+		var index = keys.length;
+		if (index < 1) throw new Error('bindAll must be passed function names');
+		while (index--) {
+			var key = keys[index];
+			obj[key] = _.bind(obj[key], obj);
+		}
+	});
+
+	_.memoize = function(func, hasher) {
+		var memoize = function(key) {
+			var cache = memoize.cache;
+			var address = '' + (hasher ? hasher.apply(this, arguments) : key);
+			if (!_.has(cache, address)) cache[address] = func.apply(this, arguments);
+			return cache[address];
+		};
+		memoize.cache = {};
+		return memoize;
+	}
+
+	_.delay = restArgs(function(func, wait, args) {
+		return setTimeout(function(){
+			return func.apply(null,args);
+		},wait);
+	});
+
+	_.defer = _.partial(_.delay, _, 1);
+
+	_.throttle = function(func, wait, options) {
+		var timeout, context, args, result;
+		var previous = 0;
+		if (!options) options = {};
+
+		var later = function() {
+			previous = options.leading === false ? 0 : _.now();
+			timeout = null;
+			result = func.apply(context, args);
+			if (!timeout) context = args = null;
+		};
+
+		var throttled = function() {
+			var now = _.now();
+			if (!previous && options.leading === false) previous = now;
+			var remaining = wait - (now - previous);
+			context = this;
+			args = arguments;
+			if (remaining <= 0 || remaining > wait) {
+				if (timeout) {
+					clearTimeout(timeout);
+					timeout = null;
+				}
+				previous = now;
+				result = func.apply(context, args);
+				if (!timeout) context = args = null;
+			} else if (!timeout &&  options.trailing !== false) {
+				timeout = setTimeout(later, remaining);
+			}
+			return result;
+		};
+
+		throttled.cancel = function() {
+			clearTimeout(timeout);
+			previous = 0;
+			timeout = context = args = null;
+		};
+
+		return throttled;
+	};
+
+	_.debounce = function(func, wait, immediate) {
+		var timeout, result;
+
+		var later = function(context, args) {
+			timeout = null;
+			if (args) result = func.apply(context, args);
+		};
+
+		var debounced = restArgs(function(args) {
+			if (timeout) clearTimeout(timeout);
+			if (immediate) {
+				var callNow = !timeout;
+				timeout = setTimeout(later, wait);
+				if (callNow) result = func.apply(this, args);
+			} else {
+				timeout = _.delay(later, wait, this, args);
+			}
+
+			return result;
+		});
+
+		debounced.cancel = function() {
+			clearTimeout(timeout);
+			timeout = null;
+		};
+		return debounced;
+	};
+
+	_.wrap = function(func, wrapper) {
+		return _.partial(wrapper, func);
+	};
+
+	_.negate = function(predicate) {
+		return function() {
+			return !predicate.apply(this, arguments);
+		};
+	};
+
+	_.compose = function() {
+		var args = arguments;
+		var start = args.length - 1;
+		return function() {
+			var i = start;
+			var result = args[start].apply(this, arguments);
+			while (i--) result = args[i].call(this, result);
+			return result;
+		};
+	};
+
+	_.after = function(times, func) {
+		return function() {
+			if (--times < 1) {
+				return func.apply(this, arguments);
+			}
+		};
+	};
+
+	_.before = function(times, func) {
+		var memo;
+		return function() {
+			if (--times > 0) {
+				memo = func.apply(this, arguments);
+			}
+			if (times <= 1) func = null;
+			return memo;
+		};
+	};
+
+
+
+	
+
+
+
+
+
+
+
 
 
 
