62aa0b5c359d3c144418ae41f7ccf348a3f88b40
byungmin_kim <whoilgga@gmail.com>
2016-12-23 12:23:27 +0900

    add files

--- frontend/testCode.js
+++ frontend/testCode.js
@@ -1,8 +1,32 @@
-var msgArray = [];
-msgArray[0] = 'Hello';
-msgArray[99] = 'world';
-
-if (msgArray.length === 100) {
-  console.log('The length is 100');
-  console.log(msgArray[44]);
+function Person(gender) {
+  this.gender = gender;
+  console.log("person function");
+  console.log(this);
 }
+
+ Person.prototype.hello = "this is";
+
+var User = {};
+
+User = function() {
+  console.log("this");
+  console.log(this);
+};
+(function() {
+  var classUser = User();
+  console.log("User function");
+  console.log(classUser);
+})();
+
+(function() {
+  var classPerson = Person.prototype;
+  console.log("person iify");
+  console.log(classPerson);
+  
+})();
+
+var p1 = new Person('male');
+
+console.log(p1.gender);
+
+console.log(this);
